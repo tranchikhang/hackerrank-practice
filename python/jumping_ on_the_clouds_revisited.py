5e70f504ff5c1dd3fb68536c06ea7eb22264903b
tranchikhang <tranchikhang@outlook.com>
2023-05-21 16:25:51 +0900

    Add solutions

---
+++ python/jumping_ on_the_clouds_revisited.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c, k):
+    energy = 100
+    current_pos=0
+    while True:
+        current_pos = (current_pos + k)%len(c)
+        energy = energy-1
+        if c[current_pos] == 1:
+            # thunder
+            energy = energy -2
+        if current_pos==0:
+            break
+    return energy
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
