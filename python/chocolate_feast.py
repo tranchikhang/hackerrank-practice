c13c3693202c80a974ae0e0ff5225b0d6d6ac9f5
tranchikhang <tranchikhang@outlook.com>
2024-02-08 00:07:28 +0900

    Add solutions

---
+++ python/chocolate_feast.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'chocolateFeast' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER c
+#  3. INTEGER m
+#
+
+def chocolateFeast(n, c, m):
+    # Write your code here
+    total = 0
+    # chocolate in hand = wrap
+    in_hand = n//c
+    total += in_hand
+    while in_hand>=m:
+        can_trade = in_hand//m
+        total += can_trade
+        in_hand = in_hand + can_trade - can_trade*m
+    return total
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        c = int(first_multiple_input[1])
+
+        m = int(first_multiple_input[2])
+
+        result = chocolateFeast(n, c, m)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
